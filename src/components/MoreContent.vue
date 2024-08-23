ed3e566c439d53bbf147ebc98b649e4943373264
hwxlikemi <159146868+hwxlikemi@users.noreply.github.com>
2024-08-23 12:47:39 +0800

    Update MoreContent.vue

--- src/components/MoreContent.vue
+++ src/components/MoreContent.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="more-content">时光过得如此之快，很快就开学了，所以从即日（8月24日）起，该主页不再频繁维护。</div>
+  <div class="more-content">时光过得如此之快，很快就开学了，所以，从即日（8月24日）起，该主页不再频繁维护。</div>
 </template>
 
 <style lang="scss" scoped>
