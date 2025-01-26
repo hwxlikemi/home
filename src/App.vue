27e389b22719e1466bddfe407e50dc400810ac70
hwxlikemi <159146868+hwxlikemi@users.noreply.github.com>
2025-01-26 20:43:53 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -83,7 +83,7 @@ onMounted(() => {
   // 屏蔽右键
   document.oncontextmenu = () => {
     ElMessage({
-      message: "为了浏览体验，本站禁用右键",
+      message: "右键被hwxlikemi禁用",
       grouping: true,
       duration: 2000,
     });
